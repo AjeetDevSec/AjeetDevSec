59024ddfc27ca1ae8b9997f6803ff37833bfbff1
AjeetDevSec <ajeet@devseccops.ai>
2023-11-18 11:09:29 +0530

    initial test

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3005;
 
-
 app.listen(
   PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on Port ${PORT}`)
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on this port Port ${PORT}`
+  )
 );
